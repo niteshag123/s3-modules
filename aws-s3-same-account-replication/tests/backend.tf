04f1413a975359e04006cf849e75bbdebd36c45f
Nitesh.Agarwal <nitesh.agarwal@ihsmarkit.com>
2023-09-27 14:21:26 +0530

    Deleteing modules

--- aws-s3-same-account-replication/tests/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {}
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
--- aws-s3-same-account-replication/tests/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
-}
-
-provider "aws" {
-  alias  = "london"
-  region = "eu-west-2"
-}
